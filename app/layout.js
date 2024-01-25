a8b19c603ec6257d02b200ae27c2d201ea5aa400
Eren Tın <erentin@outlook.com>
2024-01-25 11:43:45 +0300

    Header, Footer, BlogCard Created.

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,21 @@
-import { Inter } from "next/font/google";
+import { Inter } from '@next/font/google';
 import "./globals.css";
+import Head from './head';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import styles from './styles.module.css'
 
+const interFont = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={interFont.className}>
+      <Head />
+      <body className={styles.container}>
+        <Header />
+        {children}
+        <Footer />
+        </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
-import Image from "next/image";
+import BlogCard from '../components/BlogCard'
 
 export default function Home() {
-  return <div>Home</div>;
+  return <div>
+    <BlogCard />
+  </div>;
 }
